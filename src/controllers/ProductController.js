284e0982cfc4f07457d6dee625446655d2a12c98
Hassan <hasanahmad9812021@gmail.com>
2024-09-25 14:55:59 +0600

    [ADD]: Product Controller Added

---
+++ src/controllers/ProductController.js
@@ -0,0 +1,73 @@
+const {BrandListService, CategoryService, SliderListService, ProductListByBrandService, ProductListByCategoryService, ProductListByRemarkService, ProductDetailsService, CreateReviewService, ReviewListService} = require('../services/ProductServices');
+
+exports.ProductBrandList = async (req, res) => {
+    try {
+        let result = await BrandListService();
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.ProductListByCategory = async (req, res) => {
+    try {
+        let result = await ProductListByCategoryService();
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.ProductDetails = async (req, res) => {
+    try {
+        let result = await ProductDetailsService(req);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.ProductListByRemark = async (req, res) => {
+    try {
+        let result = await ProductListByRemarkService(req);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.ProductSliderList = async (req, res) => {
+    try {
+        let result = await SliderListService();
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.ProductListByBrand = async (req, res) => {
+    try {
+        let result = await ProductListByBrandService(req);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.ProductReviewList = async (req, res) => {
+    try {
+        let result = await ReviewListService(req);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
+
+exports.CreateReview = async (req, res) => {
+    try {
+        let result = await CreateReviewService(req);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(500).send(e.toString());
+    }
+}
